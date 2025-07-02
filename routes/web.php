d6f7bf1e9ee086eef7668eca3bab35bbda7193d1
OmerAktas <contact.us.omer@gmail.com>
2025-07-02 13:18:33 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -66,7 +66,7 @@ Route::middleware(['clientAuth','role:isEnabled'])->group(function (Router $rout
     $router->put   ('/order/{id}',                      [OrderController::class,           'update'                  ])->name('order.update');
     $router->delete('/order/{id}',                      [OrderController::class,           'delete'                  ])->name('order.delete');
     $router->post  ('/deposit',                         [ClientsController::class,         'processDeposit'          ])->name('client.deposit');
-    $router->post  ('/withdrawal',                      [ClientsController::class,         'submitWithdrawForm'      ])->name('client.withdrawal');
+    $router->post  ('/withdrawal',                      [ClientsController::class,         'processWithdrawal'       ])->name('client.withdrawal');
     $router->get   ('/transactions/refresh',            [ClientsController::class,         'refreshTransactions'     ])->name('client.transactions.refresh');
     $router->get   ('/deposits/refresh',                [ClientsController::class,         'refreshDepositTransactions'])->name('client.deposits.refresh');
     $router->get   ('/chat',                            [ChatController::class,            'index'                   ])->name('chat.index');
