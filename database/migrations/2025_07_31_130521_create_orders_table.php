9d2b0b1285d628d623f93db70d0a57d713f5fec9
OmerAktas <contact.us.omer@gmail.com>
2025-07-31 13:10:40 +0300

    Update 2025_07_31_130521_create_orders_table.php

--- database/migrations/2025_07_31_130521_create_orders_table.php
+++ database/migrations/2025_07_31_130521_create_orders_table.php
@@ -11,21 +11,20 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('broker_id')->nullable();
-            $table->unsignedBigInteger('currency')->nullable(); 
+            $table->unsignedBigInteger('currency')->nullable();
             $table->decimal('amount', 12, 2)->nullable();
             $table->decimal('open_price', 12, 6)->nullable();
             $table->decimal('close_price', 12, 6)->nullable();
             $table->decimal('pnl', 12, 6)->nullable();
             $table->decimal('s_l', 12, 6)->nullable();
             $table->decimal('s_p', 12, 6)->nullable();
-            $table->string('type')->nullable(); // buy/sell
-            $table->string('status')->default('active');
+            $table->string('type')->nullable();
+            $table->string('status')->default('active'); // لو ضايفها هنا عادي
             $table->string('ref_currency')->nullable();
             $table->decimal('required_margin', 12, 2)->nullable();
             $table->text('comment')->nullable();
-            // $table->timestamp('created_at')->nullable();
-            $table->timestamp('closed_at')->nullable();
-            $table->timestamps();
+            $table->timestamp('closed_at')->nullable(); // دي بس اللي مش موجودة في timestamps
+            $table->timestamps(); // دي بتضيف created_at و updated_at تلقائي
         });
     }
 
