7cd7718cec8b38f0f6beb5d9ec5eeffb19dbcdae
DESKTOP-0OLL37O\anb_s <anb_syria@hotmail.com>
2026-04-12 12:46:15 +0300

    Amer

--- app/Http/Controllers/ClientLoginController.php
+++ app/Http/Controllers/ClientLoginController.php
@@ -129,7 +129,8 @@ class ClientLoginController extends Controller
     }
     
     private function createBrokerId() {
-        $lastBrokerId = Client::max('broker_id');
+        //$lastBrokerId = Client::max('broker_id');
+        $lastBrokerId = where('pipeline_id', config('app.pipeline_id'))->max('broker_id');
         return $lastBrokerId + 1;
     }
 
